7465e0e2bffd3a0af32151a17343727b79999f2b
vikas <Vikasb@corruscloud.com>
2025-03-19 10:12:33 +0530

    .

--- app/SubmitTodo.tsx
+++ app/SubmitTodo.tsx
@@ -86,7 +86,7 @@ const SubmitTodo: React.FC<SubmitButtonProps> = ({
       {/* Task Form */}
       {showForm && (
      
-          <View className=" h-screen flex-row items-end bottom-0 justify-center absolute w-screen bg-opacity-25 bg-[#000000]">
+          <View className=" h-screen flex-row items-end bottom-0 justify-center absolute w-screen bg-[rgba(0,0,0,0.35)]">
             <View className="bg-white rounded-t-xl w-full p-2">
               {/* Header */}
               <View className="flex-row justify-between items-center p-1 mb-4">
